b53d4571a3f2ce1c1c327b47cc8aca2bc728ba2f
sraviteja99 <49226953+sraviteja99@users.noreply.github.com>
2020-05-28 22:07:36 +0530

    Add files via upload
    maven project and OOP

---
+++ App.java
@@ -0,0 +1,13 @@
+//package newyear.gift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
